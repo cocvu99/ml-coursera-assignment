6b0e760b77e5231d59080dafe18249667f836997
CocVu <cocvu99@users.noreply.github.com>
2020-10-25 11:13:22 +0700

    upload week 2-3-4 exercise - NOT DONE

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -17,9 +17,9 @@ h = X * theta ;
 
 error = h - y ;
 
-error_sqr = error.^2
+error_sqr = error.^2;
 
-J = (1/(2*m))* sum([error_sqr])
+J = (1/(2*m))* sum([error_sqr]);
 % =========================================================================
 
 end
